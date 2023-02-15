314c5e2450e265f89ef2fadf1f60eaaebc68d232
PCTOMaggioli <you@example.com>
2023-02-15 10:40:27 +0100

    Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DataAnalysisComponent } from './components/data-analysis/data-analysis.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    DataAnalysisComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
