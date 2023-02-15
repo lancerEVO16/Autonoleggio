5ac1c06b8795240fa22b5c80a5a144a72e49c6a3
Mattia Giulianelli <utentesconosciuto002@gmail.com>
2023-02-15 10:54:48 +0100

    Creazione modello classe auto

---
+++ src/app/models/macchina.ts
@@ -0,0 +1,11 @@
+export class Macchina{
+    constructor(
+        public id : number,
+        public costruttore : string,
+        public modello : string,
+        public tipologia : string,
+        public prezzo : number,
+        public optionals : string[],
+        public immagine : string,
+        ){};
+}
