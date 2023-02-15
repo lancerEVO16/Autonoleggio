8fbd8a2c454d1e6151d1ee7689f9f75528bfe87b
Mattia Giulianelli <utentesconosciuto002@gmail.com>
2023-02-15 15:44:07 +0100

    inserimento immagini

--- src/app/services/macchine.service.ts
+++ src/app/services/macchine.service.ts
@@ -5,20 +5,20 @@ import { Injectable } from "@angular/core";
 export class MacchinaService {
 
     private flotta : Macchina[] = [
-        new Macchina(1, "Alfa Romeo", "Giulietta", "Berlina", 80, ["sedili sportivi"], "" ),
-        new Macchina(2, "Audi", "A5", "Berlina", 110, ["cambio automatico", "sedili riscaldati"], "" ),
-        new Macchina(3, "Audi", "A7", "Berlina", 170, ["cambio automatico", "seili riscaldati", "sedili in pelle"], "" ),
-        new Macchina(4, "BMW", "5", "Berlina", 120, ["sedili riscaldati", "tettuccio panoramico"], "" ),
-        new Macchina(5, "BMW", "840i", "Cabrio", 250, ["tettuccio automatico", "cambio automatico", "Apple Car Play"], "" ),
-        new Macchina(6, "BMW", "X1", "SUV", 100, ["sedili in pelle"], "" ),
-        new Macchina(7, "BMW", "X3", "SUV", 120, ["tettuccio panoramico", "sedili in pelle", "sedili riscaldati"], "" ),
-        new Macchina(8, "Fiat", "Qubo", "Pulmino", 70, ["pianale di carico"], "" ),
-        new Macchina(9, "Jeep", "Renegade", "SUV", 80, ["4x4", "gancio di traino"], "" ),
-        new Macchina(10, "Volkswagen", "Beetle", "Cabrio", 130, ["capottina in pelle"], "" ),
-        new Macchina(11, "Volkswagen", "Caravelle", "Pulmino", 120, ["pianale di carico", "sedili rimovibili"], "" ),
-        new Macchina(12, "Volkswagen", "Golf", "Berlina", 80, [], "" ),
-        new Macchina(13, "Volkswagen", "T-Roc", "Cabrio", 150, ["tettuccio ", ""], "" ),
-        new Macchina(14, "Volkswagen", "Touran", "Pulmino", 125, ["7 posti"], "" ),
+        new Macchina(1, "Alfa Romeo", "Giulietta", "Berlina", 80, ["sedili sportivi"], "alfa-giuletta-berlina-80.png" ),
+        new Macchina(2, "Audi", "A5", "Berlina", 110, ["cambio automatico", "sedili riscaldati"], "audi-a5-berlina-110.png" ),
+        new Macchina(3, "Audi", "A7", "Berlina", 170, ["cambio automatico", "seili riscaldati", "sedili in pelle"], "audi-a7-berlina-170.png" ),
+        new Macchina(4, "BMW", "5", "Berlina", 120, ["sedili riscaldati", "tettuccio panoramico"], "bmw-5-berlina-120.png" ),
+        new Macchina(5, "BMW", "840i", "Cabrio", 250, ["tettuccio automatico", "cambio automatico", "Apple Car Play"], "bmw-840i-cabrio-250.png" ),
+        new Macchina(6, "BMW", "X1", "SUV", 100, ["sedili in pelle"], "bmw-x1-suv-100.png" ),
+        new Macchina(7, "BMW", "X3", "SUV", 120, ["tettuccio panoramico", "sedili in pelle", "sedili riscaldati"], "bmw-x3-suv-120.png" ),
+        new Macchina(8, "Fiat", "Qubo", "Pulmino", 70, ["pianale di carico"], "fiat-qubo-pulmino-70.png" ),
+        new Macchina(9, "Jeep", "Renegade", "SUV", 80, ["4x4", "gancio di traino"], "jeep-renegade-suv-80.png" ),
+        new Macchina(10, "Volkswagen", "Beetle", "Cabrio", 130, ["capottina in pelle"], "vw-beetle-cabrio-130.png" ),
+        new Macchina(11, "Volkswagen", "Caravelle", "Pulmino", 120, ["pianale di carico", "sedili rimovibili"], "vw-caravelle-pulmino-120.png" ),
+        new Macchina(12, "Volkswagen", "Golf", "Berlina", 80, [], "vw-golf-berlina-80.png" ),
+        new Macchina(13, "Volkswagen", "T-Roc", "Cabrio", 150, ["tettuccio automatico"], "vw-t-roc-cabrio-150.png" ),
+        new Macchina(14, "Volkswagen", "Touran", "Pulmino", 125, ["7 posti"], "vw-touran-pulmino-125.png" ),
     ]
     
     GetAll(): Macchina[] { return this.flotta; }
